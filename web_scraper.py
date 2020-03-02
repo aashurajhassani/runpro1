4994e906c07955be05a0b5fc05628e32296198e9
tusharnandy <55907266+tusharnandy@users.noreply.github.com>
2020-03-02 12:49:34 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -6,8 +6,11 @@ uClient =  uReq(my_url)
 page_html = uClient.read()
 page_soup = soup(page_html, "html.parser")
 #print(page_soup.p)
-containers = page_soup.findAll("div", {"class":"item-info"})
-#print(containers[1])
-container  = containers[0]
-title_container = container.findAll("span", {"class":"price-save-percent"})
-print(title_container[0].text)
+containers = page_soup.findAll("div", {"class":"item-branding"})
+
+brand = input("Enter the brand name: ").lower()
+count = 0
+for i in range(0, len(containers)):
+    if brand == (containers[i].img['alt']).lower():
+        count += 1
+print(count)
